c647acdbd6622d108e28c8dd7f3df764f6180204
karel <k.l.kubat@gmail.com>
2024-06-19 18:18:22 +0100

    feat: add extension_format

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use pgrx::prelude::*;
 
 pgrx::pg_module_magic!();
@@ -9,10 +9,15 @@ pgrx::pg_module_magic!();
 /// - "evm"
 /// - "cosmos"
 #[pg_extern(immutable, parallel_safe)]
-pub fn decode_transfer_packet(input: &[u8], rpc_type: &str, throws: bool) -> pgrx::JsonB {
+pub fn decode_transfer_packet(
+    input: &[u8],
+    rpc_type: &str,
+    throws: bool,
+    extension_format: &str,
+) -> pgrx::JsonB {
     let result = match rpc_type {
-        "evm" => decode_from_eth_abi(input),
-        "cosmos" => decode_from_proto(input),
+        "evm" => decode_from_eth_abi(input, extension_format),
+        "cosmos" => decode_from_proto(input, extension_format),
         _ => unimplemented!("only rpc types evm and cosmos are implemented"),
     };
 
@@ -23,7 +28,7 @@ pub fn decode_transfer_packet(input: &[u8], rpc_type: &str, throws: bool) -> pgr
     result.unwrap_or(pgrx::JsonB(serde_json::Value::Null))
 }
 
-fn decode_from_eth_abi(input: &[u8]) -> Result<pgrx::JsonB> {
+fn decode_from_eth_abi(input: &[u8], extension_format: &str) -> Result<pgrx::JsonB> {
     use alloy_sol_types::{sol, SolType};
     use serde::Serialize;
 
@@ -44,14 +49,26 @@ fn decode_from_eth_abi(input: &[u8]) -> Result<pgrx::JsonB> {
     }
 
     let packet = RelayPacket::abi_decode_params(input, false)?;
-    Ok(pgrx::JsonB(serde_json::to_value(&packet)?))
+
+    let data = match extension_format {
+        "string" => serde_json::to_value(&packet)?,
+        "json" => {
+            let extension: serde_json::Value = serde_json::from_str(&packet.extension)?;
+            let mut packet = serde_json::to_value(&packet)?;
+            packet["extension"] = extension;
+            packet
+        }
+        _ => bail!("unknown extension format {extension_format}"),
+    };
+
+    Ok(pgrx::JsonB(data))
 }
 
-fn decode_from_proto(input: &[u8]) -> Result<pgrx::JsonB> {
+fn decode_from_proto(input: &[u8], extension_format: &str) -> Result<pgrx::JsonB> {
     let mut value: serde_json::Value = serde_json::from_slice(input)?;
     // PFM is sometimes a stringified JSON, in that case we transform it into the JSON object.
     if let Some(extension) = value.get_mut("extension") {
-        if extension.is_string() {
+        if extension.is_string() && (extension_format == "json") {
             let payload: serde_json::Value = match serde_json::from_str(extension.as_str().unwrap())
             {
                 // Do nothing if we cannot decode.
@@ -74,10 +91,10 @@ mod tests {
     #[test]
     fn test_decode_transfer_packet_evm() {
         let data = BASE64_STANDARD.decode("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAeAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFCcVbrZxmEMErnXaSa1gxEebSQoGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACoweDBlNGFhZjEzNTFkZTRjMDI2NGM1YzcwNTZlZjM3NzdiNDFiZDhlMDMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMV7ImZvcndhcmQiOnsicmVjZWl2ZXIiOiIyNzE1NkViNjcxOTg0MzA0YWU3NURhNDlhRDYwQzQ0NzlCNDkwQTA2IiwicG9ydCI6Indhc20udW5pb24xbTM3Y3hsMGxkNHVhdzNyNGx2OW50MnV3Njl4eGY4eGZqcmY3YTR3OWhhbXY2eHZwNmRkcXFmYWFhYSIsImNoYW5uZWwiOiJjaGFubmVsLTcxIiwidGltZW91dCI6IjAiLCJyZXRyaWVzIjogMCB9fQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==").unwrap();
-        let json = decode_transfer_packet(&data, "evm", true);
+        let json = decode_transfer_packet(&data, "evm", true, "json");
         assert_eq!(
             json.0,
-            json!({"extension":"{\"forward\":{\"receiver\":\"27156Eb671984304ae75Da49aD60C4479B490A06\",\"port\":\"wasm.union1m37cxl0ld4uaw3r4lv9nt2uw69xxf8xfjrf7a4w9hamv6xvp6ddqqfaaaa\",\"channel\":\"channel-71\",\"timeout\":\"0\",\"retries\": 0 }}","receiver":"0x01","sender":"0x27156eb671984304ae75da49ad60c4479b490a06","tokens":[{"amount":100,"denom":"0x0e4aaf1351de4c0264c5c7056ef3777b41bd8e03"}]})
+            json!({"extension": {"forward":{"receiver":"27156Eb671984304ae75Da49aD60C4479B490A06","port":"wasm.union1m37cxl0ld4uaw3r4lv9nt2uw69xxf8xfjrf7a4w9hamv6xvp6ddqqfaaaa","channel":"channel-71","timeout":"0","retries": 0 }},"receiver":"0x01","sender":"0x27156eb671984304ae75da49ad60c4479b490a06","tokens":[{"amount":100,"denom":"0x0e4aaf1351de4c0264c5c7056ef3777b41bd8e03"}]})
         )
     }
 
@@ -87,13 +104,15 @@ mod tests {
             &serde_json::to_vec(&json!({"extension": 1})).unwrap(),
             "cosmos",
             true,
+            "json",
         );
         assert_eq!(json.0, json!({"extension": 1}));
 
         let json = decode_transfer_packet(
-            &serde_json::to_vec(&json!({"extension": "{\"foo\": 1}"})).unwrap(),
+            &serde_json::to_vec(&json!({"extension": {"foo": 1}})).unwrap(),
             "cosmos",
             true,
+            "json",
         );
         assert_eq!(json.0, json!({"extension": {"foo": 1}}));
     }
