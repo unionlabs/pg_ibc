148149a5d97f84c3afba82133cfa84a15882190b
karel <k.l.kubat@gmail.com>
2024-06-19 10:37:08 +0100

    feat: support evm packet decoding

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "Pg_ibc helps decoding IBC packets and more";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+  };
+  outputs = inputs@{ self, nixpkgs, crane, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      flake = {
+        lib = {
+          buildPgIbcExtension = pkgs: postgresql: (pkgs.buildPgrxExtension {
+            inherit postgresql;
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            name = "pg_ibc";
+            doCheck = false;
+          });
+        };
+      };
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { self', system, lib, config, pkgs, ... }:
+        let
+          craneLib = crane.lib.${system};
+        in
+        {
+          packages = rec {
+            default = pg_pfm_16;
+            pg_pfm_14 = self.lib.buildPgIbcExtension pkgs pkgs.postgresql_14;
+            pg_pfm_15 = self.lib.buildPgIbcExtension pkgs pkgs.postgresql_15;
+            pg_pfm_16 = self.lib.buildPgIbcExtension pkgs pkgs.postgresql_16;
+          };
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+          devShells.default = craneLib.devShell {
+            checks = self.checks.${system};
+            inputsFrom = with pkgs; [
+              postgresql_12
+              postgresql_13
+              postgresql_14
+              postgresql_15
+              postgresql_16
+            ];
+            packages = with pkgs; [
+              cargo-pgrx
+              postgresql
+              libiconv
+              pkg-config
+            ];
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+            PGRX_PG_SYS_SKIP_BINDING_REWRITE = "1";
+            BINDGEN_EXTRA_CLANG_ARGS = [
+              ''-I"${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.llvmPackages.libclang.version}/include"''
+            ] ++ (if pkgs.stdenv.isLinux then [
+              "-I ${pkgs.glibc.dev}/include"
+            ] else [ ]);
+          };
+        };
+    };
+}
---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+use anyhow::{bail, Result};
+use pgrx::prelude::*;
+
+pgrx::pg_module_magic!();
+
+/// Attempts to decode a packet into a JSONB. Set throws to `true` to panic on error.
+#[pg_extern(immutable, parallel_safe)]
+pub fn decode_transfer_packet(input: &[u8], rpc_type: &str, throws: bool) -> pgrx::JsonB {
+    let result = match rpc_type {
+        "evm" => decode_from_eth_abi(input),
+        "cosmos" => decode_from_proto(input),
+        _ => unimplemented!("only rpc types evm and cosmos are implemented"),
+    };
+
+    if throws {
+        return result.unwrap();
+    }
+
+    result.unwrap_or(pgrx::JsonB(serde_json::Value::Null))
+}
+
+fn decode_from_eth_abi(input: &[u8]) -> Result<pgrx::JsonB> {
+    use alloy_sol_types::{sol, SolType};
+    use serde::Serialize;
+
+    sol! {
+        #[derive(Serialize)]
+        struct Token {
+            string denom;
+            uint128 amount;
+        }
+
+        #[derive(Serialize)]
+        struct RelayPacket {
+            bytes sender;
+            bytes receiver;
+            Token[] tokens;
+            string extension;
+        }
+    }
+
+    let packet = RelayPacket::abi_decode_params(input, false)?;
+    Ok(pgrx::JsonB(serde_json::to_value(&packet)?))
+}
+
+fn decode_from_proto(_: &[u8]) -> Result<pgrx::JsonB> {
+    bail!("proto not yet supported")
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+// #[pg_schema]
+mod tests {
+    use super::*;
+    use base64::prelude::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_log_to_jsonb() {
+        let data = BASE64_STANDARD.decode("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAeAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFCcVbrZxmEMErnXaSa1gxEebSQoGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACoweDBlNGFhZjEzNTFkZTRjMDI2NGM1YzcwNTZlZjM3NzdiNDFiZDhlMDMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMV7ImZvcndhcmQiOnsicmVjZWl2ZXIiOiIyNzE1NkViNjcxOTg0MzA0YWU3NURhNDlhRDYwQzQ0NzlCNDkwQTA2IiwicG9ydCI6Indhc20udW5pb24xbTM3Y3hsMGxkNHVhdzNyNGx2OW50MnV3Njl4eGY4eGZqcmY3YTR3OWhhbXY2eHZwNmRkcXFmYWFhYSIsImNoYW5uZWwiOiJjaGFubmVsLTcxIiwidGltZW91dCI6IjAiLCJyZXRyaWVzIjogMCB9fQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==").unwrap();
+        let json = decode_transfer_packet(&data, "evm", true);
+        assert_eq!(
+            json.0,
+            json!({"extension":"{\"forward\":{\"receiver\":\"27156Eb671984304ae75Da49aD60C4479B490A06\",\"port\":\"wasm.union1m37cxl0ld4uaw3r4lv9nt2uw69xxf8xfjrf7a4w9hamv6xvp6ddqqfaaaa\",\"channel\":\"channel-71\",\"timeout\":\"0\",\"retries\": 0 }}","receiver":"0x01","sender":"0x27156eb671984304ae75da49ad60c4479b490a06","tokens":[{"amount":100,"denom":"0x0e4aaf1351de4c0264c5c7056ef3777b41bd8e03"}]})
+        )
+    }
+}
+
+/// This module is required by `cargo pgrx test` invocations.
+/// It must be visible at the root of your extension crate.
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
